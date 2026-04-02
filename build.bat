d716029de3a0b4d68ae41d37f4c93729029de6f2
Austin Merrick <austinmerrick@onsclom.net>
2026-04-02 10:43:08 -0700

    .

--- build.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-clang --target=wasm32 -O2 -nostdlib -Wl,--no-entry -Wl,--export=seedRng -Wl,--export=generateMaze -Wl,--export=getMazeSize -Wl,--export=getMazeWalls -Wl,--export=getMazeTopBots -o maze.wasm maze.c
